04e8256f36bb33d21a2af403ff9c1f3aed010c09
arctic <icyfreeze5@gmail.com>
2014-09-01 04:00:45 -0700

    *flips table*

--- faucetnet.py
+++ faucetnet.py
@@ -8,14 +8,12 @@ def dllStartup():
 dllStartup()
 def buffer_create():
     return c_double(faucetnet.buffer_create())
+    
 def tcp_listen(port):
-    faucetnet.socket_error.argtype = c_double
-    faucetnet.socket_error.restype = c_double
     return faucetnet.tcp_listen(port)
     
 def socket_has_error(handle):
-    faucetnet.socket_has_error.restype = c_double
-    return faucetnet.socket_has_error(handle)
+    return c_double(faucetnet.socket_has_error(handle))
     
 def socket_error(handle):
     faucetnet.socket_error.restype = c_char_p
@@ -33,13 +31,51 @@ def write_ushort(handle, value):
 def write_string(handle, value):
     return c_double(faucetnet.write_string(handle, value))
     
+def udp_send(handle, host, port):
+    return c_double(faucetnet.udp_send(handle, host, port))
+    
+def set_little_endian(handle, littleEndian):
+    return c_double(faucetnet.set_little_endian(handle, littleEndian))
+    
+def write_buffer(destHandle, bufferHandle):
+    return c_double(faucetnet.write_buffer(destHandle, bufferHandle))
+def buffer_destroy(bufferHandle):
+    return c_double(faucetnet.buffer_destroy(bufferHandle))
+    
+    
+    
+    
 def writeKeyValue(handle, key, value):
     if(len(key) > 255):
         print "ERROR: KEY TOO LONG"
     if(len(value) > 65535):
         value = value[:65535]
-    write_ubyte(handle, string_length(key))
+    write_ubyte(handle, len(key))
     write_string(handle, key)
-    write_ushort(handle, string_length(value))
+    write_ushort(handle, len(value))
     write_string(handle, value)
     return 0
+    
+def parseUuid(uuidString, buffer):
+    uuidString = uuidString.lower()
+    uuidString = uuidString.replace("-", "", 4)
+    #print uuidString
+    
+    if(len(uuidString) != 32):
+        print "INVALID UUID1"
+        
+    posValueString = "0123456789abcdef"
+    
+    for i in range(0,16):
+        currentNibble = uuidString[i*2+1]
+        if(posValueString.find(currentNibble) == 0):
+            print "INVALID UUID2"
+        numericByte = (posValueString.find(posValueString)-1)*16
+        
+        currentNibble = uuidString[i*2+2]
+        if(posValueString.find(currentNibble) == 0):
+            print "INVALID UUID3"
+        numericByte += posValueString.find(posValueString)-1
+        
+        write_ubyte(buffer, numericByte)
+        return 0
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from forwarding import *
 from faucetnet import *
 from random import randint
 from player import *
+import uuid
 
 gg2dll = cdll.LoadLibrary("GG2DLL.dll")
 
@@ -11,6 +12,9 @@ global tcpListener
 global serverSocket
 global attemptPortForward
 global hostingPort
+global gg2lobbyId
+
+gg2lobbyId = buffer_create()
 
 hostingPort = 8190
 attemptPortForward = 1
@@ -56,17 +60,44 @@ class GameServer:
         players.append(serverPlayer)
         
         tcpListener = tcp_listen(hostingPort)
-        
-        if(socket_has_error(tcpListener)):
+        if(socket_has_error(tcpListener)==True):
             print "Unable to host:",socket_error(tcpListener)
             
         serverSocket = tcp_connect("127.0.0.1", hostingPort)
         
-        if(socket_has_error(serverSocket)):
+        if(socket_has_error(serverSocket)==True):
             print "Unable to connect to self. Epic fail, dude."
-    #def GameServerStep:
+    def GameServerStep(self):
+        lobbyBuffer = buffer_create();
+        #TODO set_little_endian
+        set_little_endian(lobbyBuffer, False)
         
-            
+        parseUuid("b5dae2e8-424f-9ed0-0fcb-8c21c7ca1352", lobbyBuffer)
+        write_buffer(lobbyBuffer, 1)
+        write_buffer(lobbyBuffer, gg2lobbyId)
+        write_ubyte(lobbyBuffer, 0) # TCP
+        write_ushort(lobbyBuffer, hostingPort)
+        write_ushort(lobbyBuffer, 1337)
+        write_ushort(lobbyBuffer, 1337)
+        write_ushort(lobbyBuffer, 1)
+        write_ushort(lobbyBuffer, 7)
+        writeKeyValue(lobbyBuffer, "name", "POLARIS")
+        writeKeyValue(lobbyBuffer, "game", "POLARIS")
+        writeKeyValue(lobbyBuffer, "game_short", "POLARIS")
+        writeKeyValue(lobbyBuffer, "game_ver", "4654765")
+        writeKeyValue(lobbyBuffer, "game_url", "1337")
+        writeKeyValue(lobbyBuffer, "map", "GAY_GAY")
+        write_ubyte(lobbyBuffer, len("protocol_id"))
+        write_string(lobbyBuffer, "protocol_id")
+        write_ushort(lobbyBuffer, 16)
+        write_buffer(lobbyBuffer, "de7d74f8-455c-bc1b-3731-0519c44356dc")
+        
+        udp_send(lobbyBuffer, "ganggarrison.com", 29944)
+        buffer_destroy(lobbyBuffer)
+       
+        
+        
 if __name__ == '__main__':
     s = GameServer()
-            
+    while True:
+        s.GameServerStep()
