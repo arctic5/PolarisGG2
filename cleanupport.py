c478956419a804a5599579663804adeadcff8410
arctic <icyfreeze5@gmail.com>
2014-09-07 01:19:08 -0700

    a new idea

--- cleanupport.py
+++
@@ -1,3 +0,0 @@
-from forwarding import *
-
-upnp_release_port("8190","TCP")
---
+++ networking/tcp.py
@@ -0,0 +1,29 @@
+import socket
+
+def tcp_listen(port):
+    try:
+        asdf = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        asdf.bind((127.0.0.1, port))
+        asdf.listen(1)
+        asdf.setblocking(0)
+        return asdf
+    except:
+        return 0
+def tcp_connect(ip, port):
+    try:
+        asdf = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except:
+        asdf = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    try:
+        asdf.connect((ip, port))
+        asdf.setblocking(0)
+        return asdf
+    except:
+        return 0
+# lol
+def socket_accecpt(handle):
+    socket, ip = handle.accecpt
+    return socket, ip
+def socket_send(handle, buffer):
+    pass
+    
