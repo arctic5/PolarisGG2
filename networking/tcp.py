9edc4b81416c24c605be4bcbb3a9b23310ac761f
arctic <icyfreeze5@gmail.com>
2014-09-07 04:19:40 -0700

    change stuff

--- networking/tcp.py
+++ networking/tcp.py
@@ -8,22 +8,24 @@ def tcp_listen(port):
         asdf.setblocking(0)
         return asdf
     except:
-        return 0
+        return -1
 def tcp_connect(ip, port):
     try:
         asdf = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except:
-        asdf = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        try:
+            asdf = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        except:
+            return -1
     try:
         asdf.connect((ip, port))
         asdf.setblocking(0)
         return asdf
     except:
-        return 0
+        return -1
 # lol
 def socket_accecpt(handle):
-    socket, ip = handle.accecpt
+    socket, ip = handle.accecpt 
     return socket, ip
 def socket_send(handle, buffer):
     pass
-    
