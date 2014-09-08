77d2ce72dcd2e75d5d062e0a11cf211a005185a2
arctic <icyfreeze5@gmail.com>
2014-09-08 00:35:06 -0700

    reimplemented buffers using a class so it (hopefully) works

--- networking/buffer.py
+++ networking/buffer.py
@@ -1,100 +1,59 @@
-from collections import deque
 import struct
-
-# create a dict to store buffers
-global __buffers = {}
-
+class Buffer:
+    def __init__(self):
+        self.pos = 0
+        self.bigEndian = False
+        self.bufferString = ""
+        return self.id()
+    def write(self, type, data):
+        if (self.bigEndian == False):
+            self.bufferString += str(struct.pack("<" + type, data)
+        else:
+            self.bufferString += str(struct.pack(">" + type, data)
+        return 0
+    def read(self, type):
+        if (self.bigEndian == False):
+            if (self.pos >= len(self.bufferString)):
+                return -1   
+            else:
+                r = struct.unpack_from("<" + type, self.bufferString, self.pos)
+                self.pos += 1
+                return r
+    def get_buf_string(self):
+        return self.bufferString
 def buffer_create():
-    bufId = len(__buffers) + 1
-    __buffers[bufId] = ""
-    return bufId
-def write_ubyte(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<B", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">B", data))
-    return 0
-def write_byte(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<b", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">b", data))
-    return 0
-def write_ushort(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<H", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">H", data))
-    return 0
-def write_short(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<h", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">h", data))
-    return 0
-def write_uint(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<I", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">I", data))
-    return 0
-def write_int(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<i", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">i", data))
-    return 0
-def write_float(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<f", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">f", data))
-    return 0
-def write_double(buffer, data, bigEndian=False):
-    if (bigEndian == False):
-        __buffers[buffer] += str(struct.pack("<d", data))
-    else:
-        __buffers[buffer] += str(struct.pack(">d", data))
-    return 0
-def read_ubyte(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<B", __buffers[buffer])
-    else:
-        return struct.unpack(">B", __buffers[buffer])
-def read_byte(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<b", __buffers[buffer])
-    else:
-        return struct.unpack(">b", __buffers[buffer])
-def read_ushort(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<H", __buffers[buffer])
-    else:
-        return struct.unpack(">H", __buffers[buffer])
-def read_short(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<h", __buffers[buffer])
-    else:
-        return struct.unpack(">h", __buffers[buffer])
-def read_uint(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<I", __buffers[buffer])
-    else:
-        return struct.unpack(">I", __buffers[buffer])
-def read_int(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<i", __buffers[buffer])
-    else:
-        return struct.unpack(">i", __buffers[buffer])
-def read_float(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<f", __buffers[buffer])
-    else:
-        return struct.unpack(">f", __buffers[buffer])
-def read_double(buffer, bigEndian=False):
-    if (bigEndian == False):
-        return struct.unpack("<d", __buffers[buffer])
-    else:
-        return struct.unpack(">d", __buffers[buffer])
+    return Buffer()
+def write_ubyte(buffer, data):
+    return buffer.write("B", data)
+def write_byte(buffer, data):
+    return buffer.write("b", data)
+def write_ushort(buffer, data):
+    return buffer.write("H", data)
+def write_short(buffer, data):
+    return buffer.write("h", data)
+def write_uint(buffer, data):
+    return buffer.write("I", data)
+def write_int(buffer, data):
+    return buffer.write("i", data)
+def write_float(buffer, data):
+    return buffer.write("f", data)
+def write_double(buffer, data):
+    return buffer.write("d", data)
+def read_ubyte(buffer):
+    return buffer.read("B")
+def read_byte(buffer):
+    return buffer.read("b")
+def read_ushort(buffer):
+    return buffer.read("H")
+def read_short(buffer):
+    return buffer.read("h")
+def read_uint(buffer):
+    return buffer.read("I")
+def read_int(buffer):
+    return buffer.read("i")
+def read_float(buffer):
+    return buffer.read("f")
+def read_double(buffer):
+    return buffer.read("d")
 
     
