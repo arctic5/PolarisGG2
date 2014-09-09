9f3990a0a934f138570daa280dc2c0a8ff965079
arctic <icyfreeze5@gmail.com>
2014-09-08 23:09:41 -0700

    more stuff

--- networking/buffer.py
+++ networking/buffer.py
@@ -15,9 +15,7 @@ class Buffer:
         else:
             r = struct.unpack_from(self.endianness + type, self.bufferString, self.pos)
             self.pos += struct.calcsize(self.endianness + type)
-            return r
-    def get_buf_string(self):
-        return self.bufferString
+            return r[0]
 def buffer_create():
     return Buffer()
 def write_ubyte(buf, data):
@@ -37,18 +35,18 @@ def write_float(buf, data):
 def write_double(buf, data):
     return buf.write("d", data)
 def read_ubyte(buf):
-    return buf.read("B")[0]
+    return buf.read("B")
 def read_byte(buf):
-    return buf.read("b")[0]
+    return buf.read("b")
 def read_ushort(buf):
-    return buf.read("H")[0]
+    return buf.read("H")
 def read_short(buf):
-    return buf.read("h")[0]
+    return buf.read("h")
 def read_uint(buf):
-    return buf.read("I")[0]
+    return buf.read("I")
 def read_int(buf):
-    return buf.read("i")[0]
+    return buf.read("i")
 def read_float(buf):
-    return buf.read("f")[0]
+    return buf.read("f")
 def read_double(buf):
-    return buf.read("d")[0]
+    return buf.read("d")
